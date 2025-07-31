acc4eb21c0e4c789a6fde6b1901ef1c80fd29140
Vipul Taneja <vipul@Vipuls-MacBook-Air.local>
2025-07-31 12:18:07 +0530

    remove api

--- src/routes/user/cart.js
+++ src/routes/user/cart.js
@@ -4,6 +4,6 @@ const controller = require('../../controllers/user/cart')
 const trimRequest =require('trim-request')
 const Auth=require('../../middleware/auth')
 router.post("/add-to-cart",Auth,trimRequest.all,controller.addProductToCart)
-router.get("/remove-from-cart/:id",Auth,trimRequest.all,controller.removeProductFromCart)
+router.delete("/remove-from-cart/:id",Auth,trimRequest.all,controller.removeProductFromCart)
 router.get("/get-cart",Auth,trimRequest.all,controller.getCart)
 module.exports = router;
