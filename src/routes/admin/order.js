3ff5d0d9b9429b038767b8347492a6fdc5526478
Vipul Taneja <vipul@Vipuls-MacBook-Air.local>
2025-08-02 11:03:04 +0530

    use auth

--- src/routes/admin/order.js
+++ src/routes/admin/order.js
@@ -4,7 +4,8 @@ const controller = require('../../controllers/admin/order')
 const trimRequest =require('trim-request')
 const Auth=require('../../middleware/auth')
 
-router.get("/get-orders",Auth,trimRequest.all,controller.getAllOrders)
+// router.get("/get-orders",Auth,trimRequest.all,controller.getAllOrders)
+router.get("/get-orders",trimRequest.all,controller.getAllOrders)
 
 
 
